43c68c55a44ee5af3c27f3b9a18b07d53fa7fd92
SemyonDuB <dubskikhsemyon@gmail.com>
2023-10-22 22:14:25 +0500

    init

---
+++ RecipesBackend/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+	.AddGraphQLServer();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
