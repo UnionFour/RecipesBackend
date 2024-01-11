1b25adfa93ccfe5229efc152c1a64ec417824b01
SherhUNV <unv@plm-ural.ru>
2024-01-11 19:24:03 +0500

    Оставил в пользовательском классе нужные свойства

--- RecipesBackend/DAL/Entities/User.cs
+++ RecipesBackend/DAL/Entities/User.cs
@@ -1,20 +1,18 @@
-﻿namespace RecipesBackend.DAL.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RecipesBackend.DAL.Entities
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
 
         public string NickName { get; set; }
-        public string AboutUser { get; set; }
-        public string Photo { get; set; }
-
-        public string Login { get; set; }
+        public string Email { get; set; }
         public string HashPassword { get; set; }
 
-        public DateTime CreationDate { get; set; }
-        public DateTime UpdateTime { get; set; }
-
-        public bool IsActive { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
